d36b908b200947c0cbe2365c0a28b4408a401634
unknown <shuhratillon@gmail.com>
2022-04-04 16:32:42 +0500

    auth

--- src/pages/Login/SignIn.jsx
+++ src/pages/Login/SignIn.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { useFormik } from "formik";
 import * as yup from "yup";
+import { useFormik } from "formik";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { Navigate, Link } from "react-router-dom";
 import authApi from "./../../services/authService";
@@ -8,13 +8,15 @@ import LeftSide from "./LeftSide";
 import {
   Avatar,
   Button,
-  // Checkbox,
   TextField,
-  Paper,
   Grid,
   Typography,
   InputAdornment,
   IconButton,
+  CircularProgress,
+  Backdrop,
+  Box,
+  Stack,
 } from "@mui/material";
 import { toast } from "react-toastify";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
@@ -26,7 +28,8 @@ const validationSchema = yup.object({
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = React.useState(false);
-  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const [loading, setLoading] = React.useState(false);
+  // const handleClickShowPassword = () => setShowPassword(!showPassword);
   const handleMouseDownPassword = () => setShowPassword(!showPassword);
 
   const formik = useFormik({
@@ -36,10 +39,13 @@ export default function SignIn() {
     },
     validationSchema: validationSchema,
     onSubmit: async (values) => {
+      setLoading(true);
       try {
         await authApi.login(values.login, values.password);
+        setLoading(false);
       } catch (ex) {
-        toast.error(ex.response.statusText);
+        setLoading(false);
+        toast.error(ex.response.data.message);
       }
     },
   });
@@ -48,9 +54,15 @@ export default function SignIn() {
   return (
     <>
       <Grid container>
-        <LeftSide size={4}>
+        <LeftSide size={5}>
           <Typography align="center">Аккаунтингиз йўқми?</Typography>
-          <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "10px",
+            }}
+          >
             <Link to="/signup" style={{ textDecoration: "none" }}>
               <Button variant="outlined" color="secondary">
                 Рўйхатдан ўтиш
@@ -58,79 +70,91 @@ export default function SignIn() {
             </Link>
           </div>
         </LeftSide>
-        <Grid item xs={12} sm={12} md={8} component={Paper} elevation={0}>
-          <div
-            style={{
+
+        <Grid item sm={12} md={7}>
+          <Box
+            sx={{
               display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
               alignItems: "center",
-              height: "100vh",
+              justifyContent: "center",
+              p: 2,
+              minHeight: "100vh",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography variant="h5">Кириш</Typography>
-
-            <form
-              onSubmit={formik.handleSubmit}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                width: "50%",
-              }}
-            >
-              <TextField
-                id="login"
-                name="login"
-                label="Логин"
-                margin="normal"
-                value={formik.values.login}
-                onChange={formik.handleChange}
-                error={formik.touched.login && Boolean(formik.errors.login)}
-                helperText={formik.touched.login && formik.errors.login}
-              />
-              <TextField
-                fullWidth
-                id="password"
-                name="password"
-                label="Парол"
-                type={showPassword ? "text" : "password"}
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                margin="normal"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                      >
-                        {showPassword ? <Visibility /> : <VisibilityOff />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                error={
-                  formik.touched.password && Boolean(formik.errors.password)
-                }
-                helperText={formik.touched.password && formik.errors.password}
-              />
-              <Button
-                color="primary"
-                variant="contained"
-                type="submit"
-                fullWidth
-                sx={{ mt: 2 }}
+            {/* <Stack
+              direction="column"
+              // justifyContent="center"
+              // alignItems="center"
+              spacing={2}
+            > */}
+            <form onSubmit={formik.handleSubmit} style={{}}>
+              <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
               >
-                Кириш
-              </Button>
+                <Avatar sx={{ bgcolor: "primary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">Кириш</Typography>
+                <TextField
+                  fullWidth
+                  id="login"
+                  name="login"
+                  label="Логин"
+                  value={formik.values.login}
+                  onChange={formik.handleChange}
+                  error={formik.touched.login && Boolean(formik.errors.login)}
+                  helperText={formik.touched.login && formik.errors.login}
+                />
+                <TextField
+                  fullWidth
+                  id="password"
+                  name="password"
+                  label="Парол"
+                  type={showPassword ? "text" : "password"}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onMouseDown={handleMouseDownPassword}
+                          onMouseUp={handleMouseDownPassword}
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+                <Button
+                  fullWidth
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                  // sx={{ mt: 2 }}
+                >
+                  Кириш
+                </Button>
+              </Stack>
             </form>
-          </div>
+            {/* </Stack> */}
+          </Box>
         </Grid>
       </Grid>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </>
   );
 }
